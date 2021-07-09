4d914b5fdc3bcc504e4451ac926567627c897e95
forrestpark <pjwoo3@gmail.com>
2021-07-09 18:27:50 +0900

    edited models/index.js

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ console.log(process.env[config.use_env_variable]);
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(config.use_env_variable, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
