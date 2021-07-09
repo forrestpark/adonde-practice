cb8b52d7e1512d86b8a3b38324eb74260d2c17c4
forrestpark <pjwoo3@gmail.com>
2021-07-09 18:10:39 +0900

    production config

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@
 const env = process.env;
 
 console.log(env.DATABASE_URL);
+console.log(env.NODE_ENV);
 
 const development = {
   "username": env.POSTGRES_USERNAME,
@@ -20,7 +21,12 @@ const test = {
 }
 
 const production = {
-  "use_env_variable": 'postgres://raptohzklcwxed:a8351f1190ee5bb759aa7dbfb12d351153873abc23389eaf0d6bb4d634ddb424@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dch54qith12c4r'
+  "use_env_variable": 'postgres://raptohzklcwxed:a8351f1190ee5bb759aa7dbfb12d351153873abc23389eaf0d6bb4d634ddb424@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dch54qith12c4r',
+  "username": env.PRODUCTION_USERNAME,
+  "password": env.PRODUCTION_PASSWORD,
+  "database": env.PRODUCTION_DATABASE,
+  "host": env.PRODUCTION_HOST,
+  "dialect": "postgres"
 }
 
 module.exports = {development, test, production}
