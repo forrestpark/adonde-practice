021cd542a2bada1d84232701937bfe5ed7251211
forrestpark <pjwoo3@gmail.com>
2021-07-09 18:05:19 +0900

    troubleshooting type error

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ const test = {
 }
 
 const production = {
-  "use_env_variable": "postgres://raptohzklcwxed:a8351f1190ee5bb759aa7dbfb12d351153873abc23389eaf0d6bb4d634ddb424@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dch54qith12c4r"
+  "use_env_variable": 'postgres://raptohzklcwxed:a8351f1190ee5bb759aa7dbfb12d351153873abc23389eaf0d6bb4d634ddb424@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dch54qith12c4r'
 }
 
 module.exports = {development, test, production}
