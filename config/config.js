aa9ba1ffdd57790fe6159e1d81f23e0da9856f57
forrestpark <pjwoo3@gmail.com>
2021-07-10 13:53:54 +0900

    modified config.js

--- config/config.js
+++ config/config.js
@@ -21,7 +21,8 @@ const test = {
 }
 
 const production = {
-  "use_env_variable": 'postgres://raptohzklcwxed:a8351f1190ee5bb759aa7dbfb12d351153873abc23389eaf0d6bb4d634ddb424@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dch54qith12c4r',
+  // "use_env_variable": 'postgres://raptohzklcwxed:a8351f1190ee5bb759aa7dbfb12d351153873abc23389eaf0d6bb4d634ddb424@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dch54qith12c4r',
+  "use_env_variable": env.DATABASE_URL,
   "username": env.PRODUCTION_USERNAME,
   "password": env.PRODUCTION_PASSWORD,
   "database": env.PRODUCTION_DATABASE,
