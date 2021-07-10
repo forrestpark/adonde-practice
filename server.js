cf3963d3c476acd335f4082e6dd86bcaefd7d954
forrestpark <pjwoo3@gmail.com>
2021-07-10 21:48:25 +0900

    changed port setting

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require('./app.js');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 console.log("process.env.PORT: ", process.env.PORT)
 
-app.listen( process.env.PORT || port, function() {
+app.listen( port, function() {
     console.log('Express listening on port', port);
 });
