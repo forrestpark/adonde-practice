3213614379330394a35fe1b886a0c5ac83746c75
forrestpark <pjwoo3@gmail.com>
2021-07-10 21:41:11 +0900

    console logging process.env.PORT

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./app.js');
 const port = 3000;
 
+console.log("process.env.PORT: ", process.env.PORT)
+
 app.listen( process.env.PORT || port, function() {
     console.log('Express listening on port', port);
 });
