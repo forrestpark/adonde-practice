a2b8a2f3821f5afbce7e01d988f87d221aca62cf
forrestpark <pjwoo3@gmail.com>
2021-07-10 15:30:45 +0900

    modified config.js

--- models/index.js
+++ models/index.js
@@ -19,7 +19,7 @@ let sequelize;
 if (config.use_env_variable) {
   console.log("there exists use_env_variable")
   sequelize = new Sequelize(process.env[config.use_env_variable], config, 
-    {ssl: { rejectUnauthorized: false }});
+    {dialect: "postgres", ssl: { rejectUnauthorized: false }});
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
